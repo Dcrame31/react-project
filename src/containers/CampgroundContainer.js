ffc1dc7e62bf61a1957af3ff9508f838491cf709
Diane Crame <dcrame@icloud.com>
2020-08-09 15:01:41 -0700

    updated containers

--- src/containers/CampgroundContainer.js
+++ src/containers/CampgroundContainer.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react'
 
 export default class CampgroundContainer extends Component {
+
+    state = {
+        campground: '',
+        location: ''
+    }
+
+    componentDidMount() {
+        const url = `http://localhost:3000/${this.props.location}`
+    }
+
     render() {
         return (
             <div>
--- src/containers/LocationContainer.js
+++ src/containers/LocationContainer.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react'
+import CampgroundContainer from './CampgroundContainer'
 
 export default class LocationContainer extends Component {
+
+    state = {
+        location: ''
+    }
+
     render() {
         return (
             <div>
-
+                <CampgroundContainer location={this.state.location} />
             </div>
         )
     }
