e347335653cb5243208a41831d2cfc1cd8433019
Diane Crame <dcrame@icloud.com>
2020-08-09 13:48:57 -0700

    created location and campground containers

---
+++ src/containers/CampgroundContainer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class CampgroundContainer extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
