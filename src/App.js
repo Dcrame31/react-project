d895bd6f28d6c712e8c3f9c5772c0df373358566
Diane Crame <dcrame@icloud.com>
2020-08-10 11:18:25 -0700

    deleted folder

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './components/Home';
 import CampgroundContainer from '../containers/CampgroundContainer';
 import LocationContainer from '../containers/LocationContainer';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Campground from './components/campgrounds/Campground';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Route exact path="/" component={Home} />
           <Route exact path="/campgrounds" component={CampgroundContainer} />
           <Route exact path="/locations" component={LocationContainer} />
+          <Route exact path="/campgrounds/:id" component={Campground} />
         </Switch>
         <CampgroundContainer />
         <LocationContainer />
--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import manageCampgrounds from './reducers/manageCampgrounds';
+
+let store = createStore(manageCampgrounds, applyMiddleware(thunk))
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
