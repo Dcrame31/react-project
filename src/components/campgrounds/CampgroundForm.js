197edeff977e5f22020a219c29ac9bddff5a7d97
Diane Crame <dcrame@icloud.com>
2020-08-10 19:16:42 -0700

    added seed

--- src/components/campgrounds/CampgroundForm.js
+++ src/components/campgrounds/CampgroundForm.js
@@ -12,7 +12,7 @@ export default class CampgroundForm extends Component {
                 <h1>Submit a Campground</h1>
                 <form>
                     <p>Name: <input type="text" /></p>
-                    <p>Description: <input type="text" /></p>
+                    <p>Description: <textarea /></p>
                     <p>Location: <input type="text" /></p>
                     <p><input type="button" value="Submit" /></p>
                 </form>
