d3e35afacec73a18a56d162dae39d47251529ca7
Diane Crame <dcrame@icloud.com>
2020-08-13 12:18:07 -0700

    added load location reducer

--- src/reducers/manageLocations.js
+++ src/reducers/manageLocations.js
@@ -1,9 +1,20 @@
 import React from 'react'
 
-export default function manageLocations() {
-    return (
-        <div>
-
-        </div>
-    )
+export default function manageLocations(state = {
+    locations: []
+}, action) {
+    switch (action.type) {
+        case 'LOADING_LOCATIONS':
+            return {
+                ...state,
+                locations: [...state.locations]
+            }
+        case 'LOAD_LOCATIONS':
+            return {
+                ...state,
+                locations: action.locations
+            }
+        default:
+            return state;
+    }
 }
