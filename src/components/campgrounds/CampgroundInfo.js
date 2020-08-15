fc65d19dbef30c639d4abc309bd4ef8a2d56f48d
Diane Crame <dcrame@icloud.com>
2020-08-15 12:28:48 -0700

    added component to show campground info

---
+++ src/components/campgrounds/CampgroundInfo.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+
+export default class CampgroundInfo extends Component {
+
+    state = {
+        campground: '',
+        params: ''
+    }
+
+    componentDidMount() {
+        this.fetchCampground()
+    }
+
+    fetchCampground = () => {
+        let searchTerm = parseInt(this.props.match.params.id)
+        fetch(`http://localhost:3001/campgrounds/${searchTerm}`)
+            .then(res => res.json())
+            .then(data => {
+                this.setState({
+                    campground: data,
+                    params: searchTerm
+                })
+            })
+    }
+
+
+    render() {
+        const { campground } = this.state
+        return (
+            <div>
+                <h2>{campground.name}</h2>
+                <p>{campground.description}</p>
+                <p>Location: {campground.location_id}</p>
+                <p>Cost: ${campground.cost}</p>
+            </div>
+        )
+    }
+}
