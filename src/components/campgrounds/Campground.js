95d1b3ada9b36dc68c863f8c4e8b42327819cdb7
Diane Crame <dcrame@icloud.com>
2020-08-15 12:06:36 -0700

    updated presentational components for campgrounds

--- src/components/campgrounds/Campground.js
+++ src/components/campgrounds/Campground.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 
 export default function Campground(props) {
     return (
         <div>
-
+            <Link to={`/campgrounds/${props.campground.id}`}>{props.campground.name}</Link>
         </div>
     )
 }
--- src/components/campgrounds/Campgrounds.js
+++ src/components/campgrounds/Campgrounds.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react'
 // import CampgroundList from './CampgroundList';
 import Campground from './Campground';
-import { Link } from 'react-router-dom';
-
 
 export default class Campgrounds extends Component {
     render() {
         const campgrounds = this.props.campgrounds
             .sort((a, b) => a["name"] > b["name"] ? 1 : -1)
-            .map((campground, id) => <li><Link to="/campgrounds/:id">{campground.name}</Link><Campground key={id} campground={campground} /></li>)
+            .map((campground, id) => <Campground key={id} campground={campground} />)
         return (
             <div>
 
