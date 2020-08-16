a52083f213406f2b90fe1e4230357c0c602070c0
Diane Crame <dcrame@icloud.com>
2020-08-16 15:40:23 -0700

    toggle views for edit page

--- src/components/campgrounds/CampgroundForm.js
+++ src/components/campgrounds/CampgroundForm.js
@@ -56,40 +56,75 @@ class CampgroundForm extends Component {
 
     render() {
         // console.log(this.state.campground)
+        console.log(this.props.onEditPage)
         const locationOptions = this.state.locations
             .sort((a, b) => a.name > b.name ? 1 : -1)
             .map((location, id) => <option key={id} value={location.id}>{location.name}</option>)
 
+        const { campground } = this.props
         return (
             <>
-                <div class="form">
-                    <h1>Submit a Campground</h1>
-                    <form onSubmit={this.handleSubmit}>
-                        <p>Name: <input name="name" type="text"
-                            value={this.state.campground.name}
-                            onChange={this.handleChange} /></p>
+                <div>
+                    {this.props.onEditPage === true ?
 
-                        <p>Description: <input type="textarea" name="description"
-                            value={this.state.campground.description}
-                            onChange={this.handleChange} /></p>
+                        <>
+                            <h1>Edit {campground.name}</h1>
 
-                        <p>Location: <select name="location_id" id="type"
-                            value={this.state.campground.location_id}
-                            onChange={this.handleChange}>
-                            <option value="" select="true">Select a location</option>
-                            {locationOptions}
-                        </select></p>
+                            <form onSubmit={this.handleSubmit}>
+                                <p>Name: <input name="name" type="text"
+                                    value={campground.name}
+                                    onChange={this.handleChange} /></p>
 
-                        <p>Cost: <input type="number" name="cost"
-                            value={this.state.campground.cost}
-                            onChange={this.handleChange} /></p>
+                                <p>Description: <input type="textarea" name="description"
+                                    value={campground.description}
+                                    onChange={this.handleChange} /></p>
 
-                        <p>Link: <input type="text" name="link"
-                            value={this.state.campground.link}
-                            onChange={this.handleChange} /></p>
+                                <p>Location: <select name="location_id" id="type"
+                                    value={campground.location_id}
+                                    onChange={this.handleChange}>
+                                    <option value="" select="true">Select a location</option>
+                                    {locationOptions}
+                                </select></p>
+                                <p>Cost: <input type="number" name="cost"
+                                    value={campground.cost}
+                                    onChange={this.handleChange} /></p>
 
-                        <p><input type="submit" value="Submit" /></p>
-                    </form>
+                                <p>Link: <input type="text" name="link"
+                                    value={campground.link}
+                                    onChange={this.handleChange} /></p>
+
+                                <p><input type="submit" value="Submit" /></p>
+                            </form> </> :
+                        <>
+                            <h1>Submit a Campground</h1>
+                            <form onSubmit={this.handleSubmit}>
+                                <p>Name: <input name="name" type="text"
+                                    value={this.state.campground.name}
+                                    onChange={this.handleChange} /></p>
+
+                                <p>Description: <input type="textarea" name="description"
+                                    value={this.state.campground.description}
+                                    onChange={this.handleChange} /></p>
+
+                                <p>Location: <select name="location_id" id="type"
+                                    value={this.state.campground.location_id}
+                                    onChange={this.handleChange}>
+                                    <option value="" select="true">Select a location</option>
+                                    {locationOptions}
+                                </select></p>
+
+                                <p>Cost: <input type="number" name="cost"
+                                    value={this.state.campground.cost}
+                                    onChange={this.handleChange} /></p>
+
+                                <p>Link: <input type="text" name="link"
+                                    value={this.state.campground.link}
+                                    onChange={this.handleChange} /></p>
+
+                                <p><input type="submit" value="Submit" /></p>
+                            </form>
+                        </>
+                    }
                 </div>
             </>
         )
--- src/components/campgrounds/CampgroundInfo.js
+++ src/components/campgrounds/CampgroundInfo.js
@@ -8,7 +8,7 @@ class CampgroundInfo extends Component {
 
     state = {
         showForm: false,
-        editForm: false
+        showEditForm: false
     }
 
     componentDidMount() {
@@ -17,7 +17,7 @@ class CampgroundInfo extends Component {
     }
 
     handleEdit = () => {
-        this.setState({ showForm: !this.state.showForm, editForm: true })
+        this.setState({ showForm: !this.state.showForm, showEditForm: true })
     }
 
     handleDelete = e => {
@@ -25,10 +25,14 @@ class CampgroundInfo extends Component {
             this.props.history.push("/campgrounds")
         }
         this.props.deleteCampground(e.target.value, onDeleteSuccess)
+    }
 
+    onEditSuccess() {
+        this.setState({ editForm: false })
     }
 
     render() {
+        console.log(this.props.match.params.id)
         const id = parseInt(this.props.match.params.id)
         // const display = this.state.editForm === true ?  this.state.showForm && <CampgroundForm /> : <h2>{this.props.name}</h2>
         // <p>{this.props.description}</p>
@@ -37,22 +41,23 @@ class CampgroundInfo extends Component {
         return (
             <div>
                 {
-                    this.state.editForm === true ? this.state.showForm && <CampgroundForm campground={this.props} /> :
+                    this.state.showEditForm === true ? this.state.showForm && <CampgroundForm campground={this.props} onEditPage={this.state.showEditForm} onEditSuccess={this.onEditSuccess} id={this.props.match.params.id} /> :
                         <><h2>{this.props.name}</h2>
                             <p>{this.props.description}</p>
                             <p>Cost: ${this.props.cost}/day</p>
                             <p><a href={this.props.link} target="_blank">Click for more info</a></p>
+                            <button
+                                value={id}
+                                class="button"
+                                onClick={this.handleEdit}>Edit Campground</button>
+
+                            <button
+                                value={id}
+                                class="button"
+                                onClick={this.handleDelete}>Delete Campground</button>
                         </>
                 }
-                <button
-                    value={id}
-                    class="button"
-                    onClick={this.handleEdit}>Edit Campground</button>
 
-                <button
-                    value={id}
-                    class="button"
-                    onClick={this.handleDelete}>Delete Campground</button>
             </div>
         )
     }
