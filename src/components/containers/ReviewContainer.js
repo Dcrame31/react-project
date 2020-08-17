b5065ad26502a7fbb9f0cd98867c27a69aff683e
Diane Crame <dcrame@icloud.com>
2020-08-17 14:59:59 -0700

    added state for view toggling

--- src/components/containers/ReviewContainer.js
+++ src/components/containers/ReviewContainer.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
 import Reviews from '../reviews/Reviews';
+import ReviewForm from '../reviews/ReviewForm';
 
 class ReviewContainer extends Component {
+
+    state = {
+        showReviews: false,
+        showForm: false
+    }
+
     render() {
         return (
             <div>
