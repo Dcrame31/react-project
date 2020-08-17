7a099bf7f616ed3658973d560e029c5ac02bbfb5
Diane Crame <dcrame@icloud.com>
2020-08-16 20:40:14 -0700

    added reviews

---
+++ src/components/containers/ReviewContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class ReviewContainer extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default ReviewContainer;
